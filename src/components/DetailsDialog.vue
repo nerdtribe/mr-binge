2ea7699a83811599e2d1d98b906cd2da00b459b7
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2019-02-23 03:15:31 -0500

    Add detail view layout and metadata hooks

--- src/components/DetailsDialog.vue
+++ src/components/DetailsDialog.vue
@@ -1,15 +1,51 @@
 <template>
-  <v-card color="gray lighten-1">
-    <v-card-title class="headline blue-grey darken-1" primary-title>
-      <h1>{{title}}</h1>
-      <p> {{rating}}% ({{voteCount}})</p>
-    </v-card-title>
+  <v-card>
+    <v-toolbar color="blue-grey darken-1">
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+      <v-spacer></v-spacer>
+        <v-progress-circular
+        :rotate="360"
+        :size="50"
+        :width="8"
+        :value="rating"
+        color="white"
+      >
+      {{ rating }}%
+      </v-progress-circular>
+      <v-toolbar-title class="subheading" extended>({{ voteCount }})</v-toolbar-title>
+      <v-btn flat icon color="white" class="mx-2">
+        <v-icon>delete</v-icon>
+      </v-btn>
+    </v-toolbar>
 
     <v-card-text>
-      <p>{{description}}</p>
+      <v-layout align-center justify-center column fill-height>
+        <p>{{ description }}</p>
+        <youtube :video-id="trailer"></youtube>
+      </v-layout>
     </v-card-text>
-    <youtube :video-id="trailer"></youtube>
 
+    <v-card-actions fill-height class="pa-3">
+      <v-rating
+        v-model="rating"
+        background-color="white"
+        color="yellow accent-4"
+        dense
+        half-increments
+        hover
+        size="32"
+      ></v-rating>
+      <v-spacer></v-spacer>
+      <span class="grey--text text--lighten-2 caption mr-2" fill-height>
+        <v-switch
+          label="Watched"
+          color="success"
+          :value="isWatched"
+          hide-details
+          align-center
+        ></v-switch>
+      </span>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -21,7 +57,8 @@ export default {
     'trailer',
     'rating',
     'voteCount',
-    'description'
+    'description',
+    'isWatched'
   ],
   data: () => ({
     dialog: false,
