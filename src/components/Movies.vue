1a7aceef1fd370c8b70f959ab9567e962948654a
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2019-02-23 03:14:15 -0500

    Fix layout glitch on movies page

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,32 +1,43 @@
 <template>
-  <v-app id="inspire">
-    <v-layout justify-center>
-      <v-flex xs12 md6 lg6>
-        <v-text-field
-          flat
-          solo-inverted
-          hide-details
-          prepend-inner-icon="search"
-          label="Search Local Movies"
-        ></v-text-field>
-      </v-flex>
-    </v-layout>
-    <v-container grid-list-md fluid>
-      <v-layout row wrap>
-        <v-flex v-for="movie in localMovies" :key="movie.movieId" xs4 md2 lg1 d-flex class="movie-img">
-          <v-card flat tile color="transparent">
-            <v-img :src="movie.poster" :aspect-ratio="2/3" @click.prevent="selectMovie(movie.movieId)"></v-img>
-            <v-card-actions>
-              <p class="body-1 mb-0">{{ movie.title }}</p>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-    <v-dialog v-model="isDialogDisplayed" transition="dialog-bottom-transition">
+  <v-layout justify-center column>
+    <v-flex class="pa-2">
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="search"
+        label="Search Local Movies"
+      ></v-text-field>
+    </v-flex>
+    <v-flex>
+      <v-container grid-list-md fluid>
+        <v-layout row wrap>
+          <v-flex v-for="movie in localMovies" :key="movie.movieId" xs4 md2 lg1 d-flex class="movie-img">
+            <v-card flat tile color="transparent">
+              <v-img :src="movie.poster" :aspect-ratio="2/3" @click.prevent="selectMovie(movie.movieId)"></v-img>
+              <v-card-actions>
+                <p class="body-1 mb-0">{{ movie.title }}</p>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-flex>
+    <v-btn
+      color="primary"
+      dark
+      absolute
+      bottom
+      right
+      fab
+      class="mr-4 mb-5"
+    >
+      <v-icon>add</v-icon>
+    </v-btn>
+    <v-dialog v-model="isDialogDisplayed" transition="dialog-bottom-transition" lazy>
       <DetailsDialog @cancel="isDialogDisplayed = false" @delete="isDialogDisplayed = false" v-bind="this.$store.state.selectedDetail"/>
     </v-dialog>
-  </v-app>
+  </v-layout>
 </template>
 
 <script>
