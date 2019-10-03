92a89b8b459eeb847e3c94cb997a119df312d680
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2019-10-03 16:26:15 -0400

    Add tv and movie components

---
+++ src/components/TvComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>TV Page Works!</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'TvComponent',
+  data: () => ({ }),
+});
+</script>
