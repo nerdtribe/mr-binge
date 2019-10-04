cab669418d4805b6fbbbcf6a01409b00008af3d4
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2019-10-03 22:51:21 -0400

    Add core routing, views and components

--- tests/unit/App.spec.ts
+++ tests/unit/App.spec.ts
@@ -1,12 +1,7 @@
-// Good Resources
-// https://medium.com/@lachlanmiller_52885/data-and-interaction-testing-in-vue-e7914a9179d7
-// http://iobio.io/user_guides/2019/05/20/vue-js-unit-testing-with-jest/
-// https://vue-test-utils.vuejs.org/guides/#getting-started
-// https://vuetifyjs.com/en/getting-started/unit-testing
-
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import vuetify from '@/plugins/vuetify';
 import App from '@/App.vue';
+
 
 const localVue = createLocalVue();
 localVue.use(vuetify);
@@ -14,7 +9,12 @@ localVue.use(vuetify);
 describe('Home.vue', () => {
 
   test('render', () => {
-    const wrapper = shallowMount(App);
+    const wrapper = shallowMount(App, {
+      stubs: {
+        RouterView: true,
+        TheNavbarView: true,
+      },
+    });
     expect(wrapper.isVueInstance()).toBe(true);
   });
 });
