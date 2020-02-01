f966010be3069f318d9cbdf52aa0b51737f7d735
Lance Gundersen <lance.gundersen@linuxacademy.com>
2020-02-01 16:24:43 -0500

    add Vue CLI Plugin Electron Builder

--- src/main.js
+++ src/main.js
@@ -1,22 +1,12 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-import VueYouTubeEmbed from 'vue-youtube-embed'
-import db from './datastore'
-
-Vue.prototype.$db = db
-Vue.use(VueYouTubeEmbed)
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App),
-  mounted () {
-    // Prevent blank screen in Electron builds
-    this.$router.push('/')
-  }
-}).$mount('#app')
+}).$mount('#app');
