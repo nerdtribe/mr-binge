963235c739ca55b9f8cf907e1ac49f180d8ea95d
Lance Gundersen <lance.gundersen@linuxacademy.com>
2020-02-04 10:54:28 -0500

    Add Views

---
+++ src/views/DetailView.vue
@@ -0,0 +1,14 @@
+<template>
+  <MediaDetailComponent />
+</template>
+
+<script>
+import Vue from "vue";
+import MediaDetailComponent from "../components/MediaDetailComponent.vue";
+
+export default Vue.extend({
+  components: {
+    MediaDetailComponent
+  }
+});
+</script>
---
+++ src/views/MovieView.vue
@@ -0,0 +1,14 @@
+<template>
+  <MediaComponent type="movies" />
+</template>
+
+<script>
+import Vue from "vue";
+import MediaComponent from "../components/MediaComponent.vue";
+
+export default Vue.extend({
+  components: {
+    MediaComponent
+  }
+});
+</script>
