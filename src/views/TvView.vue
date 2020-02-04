42471e830d115af4f9f2680c0fce6c417ef4e3f3
Lance Gundersen <lance.gundersen@linuxacademy.com>
2020-02-04 10:55:57 -0500

    add tv view

---
+++ src/views/TvView.vue
@@ -0,0 +1,14 @@
+<template>
+  <MediaComponent type="tv" />
+</template>
+
+<script>
+import Vue from "vue";
+import MediaComponent from "../components/MediaComponent.vue";
+
+export default Vue.extend({
+  components: {
+    MediaComponent
+  }
+});
+</script>
