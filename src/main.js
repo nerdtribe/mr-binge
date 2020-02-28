3f0160e9614d74a4d21071fe4e26bc1ec29e5434
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2020-02-27 22:39:33 -0500

    Details page layout

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
+import VueYouTubeEmbed from "vue-youtube-embed";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import db from "./store/db";
 
+Vue.use(VueYouTubeEmbed);
 Vue.config.productionTip = false;
 
 new Vue({
